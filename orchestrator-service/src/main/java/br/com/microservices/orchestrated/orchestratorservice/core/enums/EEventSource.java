6c15735b77b25206b6290a65357223725a9ff585
jpcchaves <jpcchaves@outlook.com>
2024-10-07 15:58:12 -0300

    add enum values

--- orchestrator-service/src/main/java/br/com/microservices/orchestrated/orchestratorservice/core/enums/EEventSource.java
+++ orchestrator-service/src/main/java/br/com/microservices/orchestrated/orchestratorservice/core/enums/EEventSource.java
@@ -1,3 +1,8 @@
 package br.com.microservices.orchestrated.orchestratorservice.core.enums;
 
-public enum EEventSource {}
+public enum EEventSource {
+  ORCHESTRATOR,
+  PRODUCT_VALIDATION_SERVICE,
+  PAYMENT_SERVICE,
+  INVENTORY_SERVICE
+}
