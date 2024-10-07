4dc70a225cd68b4468cd6bc78b82ef8cf9c7f179
jpcchaves <jpcchaves@outlook.com>
2024-10-07 16:04:33 -0300

    replicate status enum and dtos in all projects

---
+++ inventory-service/src/main/java/br/com/microservices/orchestrated/inventoryservice/core/dto/History.java
@@ -0,0 +1,20 @@
+package br.com.microservices.orchestrated.inventoryservice.core.dto;
+
+import br.com.microservices.orchestrated.inventoryservice.core.enums.ESagaStatus;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class History {
+
+  private String source;
+  private ESagaStatus status;
+  private String message;
+  private LocalDateTime createdAt;
+}
