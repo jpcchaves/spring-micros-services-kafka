925c7d820f29f121791de7968c67c4876406c231
jpcchaves <jpcchaves@outlook.com>
2024-12-22 18:32:27 -0300

    add mongo repositories

---
+++ order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/repository/EventRepository.java
@@ -0,0 +1,8 @@
+package br.com.microservices.orchestrated.orderservice.core.repository;
+
+import br.com.microservices.orchestrated.orderservice.core.document.Event;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EventRepository extends MongoRepository<Event, String> {}
