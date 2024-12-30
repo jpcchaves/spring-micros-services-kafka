b28866251dfac6a9f3e8acc94cbc3735018c63d2
jpcchaves <jpcchaves@outlook.com>
2024-12-30 13:49:38 -0300

    add success handling

--- payment-service/src/main/java/br/com/microservices/orchestrated/paymentservice/core/dto/Event.java
+++ payment-service/src/main/java/br/com/microservices/orchestrated/paymentservice/core/dto/Event.java
@@ -2,11 +2,13 @@ package br.com.microservices.orchestrated.paymentservice.core.dto;
 
 import br.com.microservices.orchestrated.paymentservice.core.enums.ESagaStatus;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.util.ObjectUtils;
 
 @Data
 @Builder
@@ -22,4 +24,12 @@ public class Event {
   private ESagaStatus status;
   private List<History> eventHistory;
   private LocalDateTime createdAt;
+
+  public void addToHistory(History history) {
+    if (ObjectUtils.isEmpty(eventHistory)) {
+      eventHistory = new ArrayList<>();
+    }
+
+    eventHistory.add(history);
+  }
 }
